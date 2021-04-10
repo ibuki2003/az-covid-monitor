9a0140f7266d32128c3b083cbbc45a38fd689a9c
ibuki2003 <main@fuwa.dev>
2021-04-10 20:25:23 +0900

    refactor: format automatically

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib
-import requests
-import json
 import base64
-import datetime
 import twitter
 import config
 import sqlite3
 
 
-AUTH_TOKEN = 'Basic ' + base64.b64encode(bytes(config.USERNAME + ':' + config.PASSWORD, 'ascii')).decode('ascii')
+AUTH_TOKEN = 'Basic ' + \
+    base64.b64encode(bytes(config.USERNAME + ':' +
+                           config.PASSWORD, 'ascii')).decode('ascii')
 BASE_URL = 'https://www.azabu-jh.ed.jp/gakunai/'
 GRADE_NAMES = {
     'all_students': '全校',
@@ -24,6 +23,7 @@ GRADE_NAMES = {
 NEW_PAGE_TPL = '{grade}向け: 「{title}」が追加されました'
 UPD_PAGE_TPL = '{grade}向け: 「{title}」が更新されました'
 
+
 def main():
     db = Database()
     for grade in GRADE_NAMES:
@@ -35,11 +35,11 @@ def main():
             })
             with urllib.request.urlopen(req) as res:
                 links = get_links(res.read())
-            
+
         except Exception as e:
             print(e)
             continue
-        
+
         for link in links:
             req = urllib.request.Request(link[0], None, {
                 'Authorization': AUTH_TOKEN
@@ -47,11 +47,14 @@ def main():
             with urllib.request.urlopen(req) as res:
                 cur_upd = res.headers['last-modified']
                 old_upd = db.get_last_update(link[0])
-                if old_upd == None:
-                    twitter.tweet(NEW_PAGE_TPL.format(grade=GRADE_NAMES[grade], title=link[1]))
+                if old_upd is None:
+                    twitter.tweet(NEW_PAGE_TPL.format(
+                        grade=GRADE_NAMES[grade], title=link[1]))
                 elif old_upd != cur_upd:
-                    twitter.tweet(UPD_PAGE_TPL.format(grade=GRADE_NAMES[grade], title=link[1]))
+                    twitter.tweet(UPD_PAGE_TPL.format(
+                        grade=GRADE_NAMES[grade], title=link[1]))
                 db.set_last_update(link[0], cur_upd)
+
 
 def get_links(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -60,8 +63,9 @@ def get_links(html):
             elm['href'],
             elm.find('dd', class_="text").find(text=True))
         for elm in soup
-            .find('ul', class_ = 'topicsInfo')
-            .find_all('a', href=True)]
+        .find('ul', class_='topicsInfo')
+        .find_all('a', href=True)]
+
 
 class Database:
     def __init__(self):
@@ -72,23 +76,26 @@ class Database:
         except:
             self.c.execute('create table tbl (url integer, updated text)')
             self.conn.commit()
-    
+
     def __del__(self):
         self.c.close()
         self.conn.close()
-    
+
     def get_last_update(self, url):
-        res = self.c.execute('select updated from tbl where url=?', (url,)).fetchone()
+        res = self.c.execute(
+            'select updated from tbl where url=?', (url,)).fetchone()
         if res is None:
-            self.c.execute('insert into tbl (url, updated) values (?, ?)', (url, ''))
+            self.c.execute(
+                'insert into tbl (url, updated) values (?, ?)', (url, ''))
             self.conn.commit()
             return None
         return res[0]
-    
+
     def set_last_update(self, url, updated):
-        self.get_last_update(url) # create line if not exist
+        self.get_last_update(url)  # create line if not exist
         self.c.execute('update tbl set updated=? where url=?', (updated, url,))
         self.conn.commit()
+
 
 if __name__ == "__main__":
     main()
--- twitter.py
+++ twitter.py
@@ -2,6 +2,7 @@ import config
 import requests_oauthlib
 import datetime
 
+
 def get_session():
     return requests_oauthlib.OAuth1Session(
         config.ConsumerKey,
@@ -9,12 +10,15 @@ def get_session():
         config.AccessToken,
         config.AccessSecret)
 
+
 TWEET_URL = 'https://api.twitter.com/1.1/statuses/update.json'
+
+
 def tweet(content):
     content += " @ " + datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
     data = {
         'status': content
     }
 
-    req = get_session().post(TWEET_URL, params = data)
+    req = get_session().post(TWEET_URL, params=data)
     return req.status_code == 200
